da8f63804ee8ef0ada5dfb40f97812456c10c6b5
Adnan Ozdemir <adnan.ozdemir@useinsider.com>
2025-06-01 13:29:13 +0300

    last

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -218,68 +218,17 @@ public class GameOverManager : MonoBehaviour
         // Zaman ölçeğini normale döndür
         Time.timeScale = 1f;
         
-        // Müziği yeniden başlat
+        // Müziği durdur
         if (BackgroundMusic.Instance != null)
         {
-            BackgroundMusic.Instance.PlayMusic();
+            BackgroundMusic.Instance.StopMusic();
         }
 
-        // Tüm düşmanları temizle
-        if (GameObject.FindGameObjectsWithTag("Enemy") != null)
-        {
-            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
-            foreach (GameObject enemy in enemies)
-            {
-                Destroy(enemy);
-            }
-        }
-
-        // Tüm taretleri temizle
-        if (GameObject.FindGameObjectsWithTag("Structure") != null)
-        {
-            GameObject[] turrets = GameObject.FindGameObjectsWithTag("Structure");
-            foreach (GameObject turret in turrets)
-            {
-                Destroy(turret);
-            }
-        }
-
-        // Tüm mermileri temizle
-        var bullets = FindObjectsOfType<Bullet>();
-        foreach (var bullet in bullets)
-        {
-            Destroy(bullet.gameObject);
-        }
-
-        // Tüm itemleri temizle
-        if (GameObject.FindGameObjectsWithTag("Item") != null)
-        {
-            GameObject[] items = GameObject.FindGameObjectsWithTag("Item");
-            foreach (GameObject item in items)
-            {
-                Destroy(item);
-            }
-        }
-
-        // Yöneticileri sıfırla
-        if (EnemySpawnManager.Instance != null)
-        {
-            EnemySpawnManager.Instance.ResetManager();
-        }
-
-        if (ResourceManager.Instance != null)
-        {
-            ResourceManager.Instance.ResetResources();
-        }
-
-        if (UpgradeManager.Instance != null)
-        {
-            UpgradeManager.Instance.ResetUpgrades();
-        }
-
-        // Sahneyi yeniden yükle
-        UnityEngine.SceneManagement.SceneManager.LoadScene(
-            UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex
-        );
+        // Oyunu kapat
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
     }
 } 
